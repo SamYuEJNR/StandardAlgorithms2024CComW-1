cde48038a922c36887c6f59bbfcdbe3e0f9d5541
Sam Yu <yu.s@etoncollege.org.uk>
2024-11-05 10:05:55 +0000

    Initial Commit

---
+++ StandardAlgorithms/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StandardAlgorithms
+//
+//  Created by Yu, Samuel (EJNR) on 05/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
