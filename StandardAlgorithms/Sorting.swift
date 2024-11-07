ab40a324829024a5080390c2b584a4d1693d23a7
Sam Yu <yu.s@etoncollege.org.uk>
2024-11-07 13:04:58 +0000

    Failed Sorting Test, created Searching Test (empty)

--- StandardAlgorithms/Sorting.swift
+++ StandardAlgorithms/Sorting.swift
@@ -9,27 +9,6 @@ import Foundation
 
 class Sorting {
     func bubbleSort(_ numbers: [Int]) -> [Int] {
-        var count = 0
-        var store = 0
-        var sorted : [Int] = []
-        for number in numbers{
-            if count != 0 {
-                if number > store {
-                    sorted.append(store)
-                    sorted.append(number)
-                }
-                else {
-                    sorted.append(number)
-                    sorted.append(store)
-                    var number = store
-                }
-            }
-            var store = number
-            count += 1
-            
-        }
-        return [1, 3, 4, 7, 9]
+        return sorted
     }
 }
-
-/// USE LEN OF SORTED ARRAY INSTEAD OF STORE
---
+++ StandardAlgorithmsTests/SearchingTest.swift
@@ -0,0 +1,10 @@
+//
+//  SearchingTest.swift
+//  StandardAlgorithmsTests
+//
+//  Created by Yu, Samuel (EJNR) on 07/11/2024.
+//
+
+import XCTest
+
+final class SearchingTest: XCTestCase {
